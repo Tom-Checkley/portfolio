ab3cd3ec23967448de920595db9cb0dd36f06992
Tom Checkley <tomc@element78.co.uk>
2021-02-16 19:19:42 +0000

    started dynamic components

--- public/src/app/modules/blog/directives/post-content.directive.ts
+++ public/src/app/modules/blog/directives/post-content.directive.ts
@@ -1,10 +1,10 @@
-import { Directive } from '@angular/core';
+import { Directive, ViewContainerRef } from '@angular/core';
 
 @Directive({
   selector: '[appPostContent]'
 })
 export class PostContentDirective {
 
-  constructor() { }
+  constructor(public viewContainerRef: ViewContainerRef) { }
 
 }
